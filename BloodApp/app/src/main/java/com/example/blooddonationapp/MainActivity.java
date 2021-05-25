0d5363d829639cb9096e5b28cb44a34550b474db
Irzam2101-7 <mcsf19a045@pucit.edu.pk>
2021-05-26 00:40:05 +0500

    registration

--- BloodApp/app/src/main/java/com/example/blooddonationapp/MainActivity.java
+++ BloodApp/app/src/main/java/com/example/blooddonationapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.blooddonationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, RegistrationActivity.class );
+        startActivity(intent);
     }
     public void login(){
 

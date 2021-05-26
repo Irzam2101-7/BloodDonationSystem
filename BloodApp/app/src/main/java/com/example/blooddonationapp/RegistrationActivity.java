5220b9ed4669bc0e49ad5340ce92ec28e320fc86
Irzam2101-7 <mcsf19a045@pucit.edu.pk>
2021-05-26 12:11:34 +0500

    push data

---
+++ BloodApp/app/src/main/java/com/example/blooddonationapp/RegistrationActivity.java
@@ -0,0 +1,44 @@
+package com.example.blooddonationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText nameEt, cityEt, bloodGroupEt, passwordEt, mobileEt;
+    private Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        nameEt = findViewById(R.id.name);
+        cityEt = findViewById(R.id.city);
+        bloodGroupEt = findViewById(R.id.blood_group);
+        passwordEt = findViewById(R.id.password);
+        mobileEt = findViewById(R.id.number);
+        submitButton = findViewById(R.id.submitt_button);
+        submitButton.setOnClickListener(new  View.OnClickListener() {
+            @Override
+            public void  onClick(View view) {
+                String name, city, blood_group, password, mobile;
+                name = nameEt.getText().toString();
+                city = cityEt.getText().toString();
+                blood_group = bloodGroupEt.getText().toString() ;
+                password = passwordEt.getText().toString();
+                mobile = mobileEt.getText().toString();
+                showMessage(name+"\n");
+                Toast.makeText(RegistrationActivity.this, name + " ", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    private  void  showMessage(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}

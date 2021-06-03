fafed7b97890c5ac2675f54135a7a1dafec08d5e
Irzam2101-7 <mcsf19a045@pucit.edu.pk>
2021-06-03 13:16:42 +0500

    Merge remote-tracking branch 'origin/master'

---
+++ BloodApp/app/src/main/java/com/example/blooddonationapp/Homepage.java
@@ -0,0 +1,29 @@
+package com.example.blooddonationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.Id.search_button){
+                    startActivity(new Intent(Homepage.this , SearchActivity.class));
+
+                }
+
+                return false;
+            }
+        });
+    }
+}
